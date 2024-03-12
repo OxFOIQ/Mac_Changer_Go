2939ddf77d7fb7d52e602076f5acae5af465ef67
OiQ <76553571+MedAmyyne@users.noreply.github.com>
2024-03-12 13:26:46 +0100

    Update Mac_Changer_Go.go

--- Mac_Changer_Go.go
+++ Mac_Changer_Go.go
@@ -1,6 +1,6 @@
 package main
 
-#Dependencies
+//Dependencies
 import (
 	"flag"
 	"log"
@@ -8,21 +8,21 @@ import (
 	"os/exec"
 )
 
-#Execution Command
+//Execution Command
 func exec_Command (command string , args_arr []string) (err error) {
-	#to handle many arguments
+	//to handle many arguments
 	args := args_arr
-	#to execute whole command
+	//to execute whole command
 	comm_exec := exec.Command(command,args...)
-	#Program Output
+	//Program Output
 	comm_exec.Stdout = os.Stdout
-	#Program Error
+	//Program Error
 	comm_exec.Stderr = os.Stderr 
-	#Program input
+	//Program input
 	comm_exec.Stdin = os.Stdin
-	#running command
+	//running command
 	err =comm_exec.Run()
-	#check_for_errors
+	//check_for_errors
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -34,12 +34,12 @@ func exec_Command (command string , args_arr []string) (err error) {
 
 
 func main() {
-	#handle arguments
+	//handle arguments
 	in_face := flag.String("iface" , "" , "interface you wish to change" )
 	new_Mac := flag.String("mac" , "" , "new mac address" )
-	#parsing arguments
+	//parsing arguments
 	flag.Parse()
-	#shutting down the_interface , changing_new mac address and then enable it
+	//shutting down the_interface , changing_new mac address and then enable it
 	exec_Command("sudo" , []string{"ifconfig" , *in_face , "down"})
 	exec_Command("sudo" , []string{"ifconfig" , *in_face , "hw" , "ether" , *new_Mac})
 	exec_Command("sudo" , []string{"ifconfig" , *in_face ,  "up"})
